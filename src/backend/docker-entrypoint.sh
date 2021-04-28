27d62f0d61257714d17d3a445087e1eaa89c2d8f
Seunghyuk Jeon <seunghyuk.jeon@deliveryhero.co.kr>
2021-04-28 14:20:32 +0900

    default user name changed

--- src/backend/docker-entrypoint.sh
+++ src/backend/docker-entrypoint.sh
@@ -20,9 +20,9 @@ User = get_user_model()  # get the currently active user model,
 User.objects.filter(username='admin').exists() or \
     User.objects.create_superuser('admin', 'admin@yogiyo.com', 'password1234')
 User.objects.filter(username='bob').exists() or \
-    User.objects.create_user('bob', 'nuno@yogiyo.com', 'password1234', is_staff=True)
+    User.objects.create_user('bob', 'bob@yogiyo.com', 'password1234', is_staff=True)
 User.objects.filter(username='james').exists() or \
-    User.objects.create_user('james', 'nuno@yogiyo.com', 'password1234', is_staff=True)
+    User.objects.create_user('james', 'james@yogiyo.com', 'password1234', is_staff=True)
 EOF
 
 # echo "Adding 'AWS Region Location' default data to MongoDB Collection..."
