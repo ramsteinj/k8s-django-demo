ac2a9760423505b1bbf84a0cbe71afe88c3e307f
Seunghyuk Jeon <seunghyuk.jeon@deliveryhero.co.kr>
2021-04-28 16:39:47 +0900

    tokenauthentication is enabled by default

--- src/backend/rest_api/settings.py
+++ src/backend/rest_api/settings.py
@@ -166,6 +166,12 @@ USE_TZ = True
 
 # Rest framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],    
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
