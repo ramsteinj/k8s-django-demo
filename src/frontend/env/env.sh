c37605ec8a966e27fb07decb3136fd21dc1d3751
Seunghyuk Jeon <seunghyuk.jeon@deliveryhero.co.kr>
2021-04-29 11:51:54 +0900

    POD_NAME added to frontend

--- src/frontend/env/env.sh
+++ src/frontend/env/env.sh
@@ -2,6 +2,7 @@
 
 echo "
     var env = {
+        POD_NAME: '${POD_NAME}',
         BACKEND_HOST: '${BACKEND_HOST}',
         BACKEND_PORT: '${BACKEND_PORT}',
         BACKEND_ENDPOINT: 'http://${BACKEND_HOST}:${BACKEND_PORT}'
