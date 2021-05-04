98896e781d4d4c89a9912e202f5df5c230fa4b3c
Seunghyuk Jeon <seunghyuk.jeon@deliveryhero.co.kr>
2021-05-04 16:29:26 +0900

    env.js removed from git repository

--- src/frontend/content/env.js
+++
@@ -1,8 +0,0 @@
-
-    var env = {
-        POD_NAME: '',
-        BACKEND_HOST: 'backend.yogiyo-dev.com',
-        BACKEND_PORT: '8000',
-        BACKEND_ENDPOINT: 'http://backend.yogiyo-dev.com:8000'
-    }
-
