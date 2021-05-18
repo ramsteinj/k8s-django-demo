dc27bffdbf4acf15f56ea875cb92a0c911739a12
Seunghyuk Jeon <seunghyuk.jeon@deliveryhero.co.kr>
2021-05-18 11:18:54 +0900

    --tail option removed

--- deploy-to-prod.sh
+++ deploy-to-prod.sh
@@ -4,4 +4,5 @@
 kubectl config use-context arn:aws:eks:ap-northeast-2:637107739800:cluster/yogiyo-forum
 
 # prepare to deploy application to EKS
-skaffold run -v info -p=prod --default-repo 637107739800.dkr.ecr.ap-northeast-2.amazonaws.com/yogiyo-forum --tail
+#skaffold run -v info -p=prod --default-repo 637107739800.dkr.ecr.ap-northeast-2.amazonaws.com/yogiyo-forum --tail
+skaffold run -v info -p=prod --default-repo 637107739800.dkr.ecr.ap-northeast-2.amazonaws.com/yogiyo-forum
